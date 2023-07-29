1392d4662033f6d5f841d3ceb1504d4c61ce8c88
Aidan Boisvert <aidan.boisvert@gmail.com>
2023-07-28 22:08:28 -0500

    Add files for PCI and Memory Management

---
+++ include/allocator.h
@@ -0,0 +1,12 @@
+#ifndef ALLOCATOR_H
+#define ALLOCATOR_H
+
+#include "memory.h"
+#include "stdint.h"
+#include "framebuffer.h"
+#include "paging.h"
+#include "serial.h"
+
+
+
+#endif
---
+++ include/memory.h
@@ -0,0 +1,11 @@
+#ifndef MEMORY_H
+#define MEMORY_H
+
+#include "stdint.h"
+#include "framebuffer.h"
+#include "paging.h"
+#include "serial.h"
+
+
+
+#endif
