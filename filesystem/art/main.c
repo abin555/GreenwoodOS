c2c4ae68d5b4b072b1f6cad1b6aa37c96f5c9206
Aidan Boisvert <aidan.boisvert@gmail.com>
2023-08-16 13:23:21 -0500

    Add art

---
+++ filesystem/art/main.c
@@ -0,0 +1,49 @@
+#include "libc.h"
+
+struct WINDOW *win;
+
+int main(int argc, char **argv){
+    print("Opening ART Window\n");
+    set_schedule(2);
+    win = window_open("ART");
+    uint32_t *buf = win->backbuffer;
+    uint32_t x = win->width/2;
+    uint32_t y = win->height/2;
+    int dx = 1;
+    int dy = -1;
+    uint32_t color = 0;
+    uint32_t old_color = 0;
+
+    int i = 0;
+    while(1){
+        i++;
+        buf[x + y*win->width] = old_color; 
+        old_color = color;
+        color++;
+
+        x += dx;
+        y += dy;
+        //fb_setPixel(x, y, color);
+        buf[x + y*win->width] = color;
+        
+        if(x <= 0){
+            dx = -dx;
+            x = 1;
+        }
+        if(x >= win->width){
+            dx = -dx;
+            x = win->width - 1;
+        }
+        if(y <= 0){
+            dy = -dy;
+        }
+        if(y >= win->height){
+            dy = -dy;
+            y = win->height - 1;
+        }
+        if(i > 1000){
+            window_update();
+            i = 0;
+        }
+    }
+}
