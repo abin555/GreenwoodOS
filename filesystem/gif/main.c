e1c95e39de9b5ffd752e965e7196002490bdf644
Aidan Boisvert <aidan.boisvert@gmail.com>
2024-12-28 17:00:39 -0600

    Mostly working GIF implementation, need to handle 'restore previous' gcd mode

--- filesystem/gif/main.c
+++ filesystem/gif/main.c
@@ -47,123 +47,41 @@ uint32_t *viewbuf;
 GifRowType *ScreenBuffer;
 
 void GIF_CopyImageToViewport(GifFileType *gif, int image_idx){
-	int i, j, Row, Col, Width, Height, ExtCode, Count;
-	GifRecordType RecordType;
-	GifByteType *Extension;
-
-	static const int InterlacedOffset[] = {
-	    0, 4, 2, 1}; /* The way Interlaced image should. */
-	static const int InterlacedJumps[] = {
-	    8, 8, 4, 2}; /* be read - offsets and jumps... */
-
-	do {
-		if (DGifGetRecordType(gif, &RecordType) == GIF_ERROR) {
-			print_serial("[GIF] Exit Error 1\n");
-			exit(1);
-		}
-		switch (RecordType) {
-		case IMAGE_DESC_RECORD_TYPE:
-			print_serial("[GIF] Get Image Desc Record\n");
-			if (DGifGetImageDesc(gif) == GIF_ERROR) {
-				print_serial("[GIF] Exit Error 2\n");
-				exit(1);
-			}
-			Row = gif->Image
-			          .Top; /* Image Position relative to Screen. */
-			Col = gif->Image.Left;
-			Width = gif->Image.Width;
-			Height = gif->Image.Height;
-			if (gif->Image.Left + gif->Image.Width >
-			        gif->SWidth ||
-			    gif->Image.Top + gif->Image.Height >
-			        gif->SHeight) {
-				print_serial(
-				        "Image is not confined to screen "
-				        "dimension, aborted.\n");
-				exit(1);
-			}
-			if (gif->Image.Interlace) {
-				/* Need to perform 4 passes on the images: */
-				for (Count = i = 0; i < 4; i++) {
-					for (j = Row + InterlacedOffset[i];
-					     j < Row + Height;
-					     j += InterlacedJumps[i]) {
-						if (DGifGetLine(
-						        gif,
-						        &ScreenBuffer[j][Col],
-						        Width) == GIF_ERROR) {
-
-							print_serial("[GIF] Exit Error 3\n");
-							exit(1);
-						}
-					}
-				}
-			} else {
-				for (i = 0; i < Height; i++) {
-					if (DGifGetLine(
-					        gif,
-					        &ScreenBuffer[Row++][Col],
-					        Width) == GIF_ERROR) {
-
-						print_serial("[GIF] Exit Error 4\n");
-						exit(1);
-					}
-				}
-			}
-			break;
-		case EXTENSION_RECORD_TYPE:
-			/* Skip any extension blocks in file: */
-			if (DGifGetExtension(gif, &ExtCode, &Extension) ==
-			    GIF_ERROR) {
-				
-				print_serial("[GIF] Exit Error 5\n");
-				exit(1);
-			}
-			while (Extension != NULL) {
-				if (DGifGetExtensionNext(gif, &Extension) ==
-				    GIF_ERROR) {
-					print_serial("[GIF] Exit Error 6\n");
-					exit(1);
-				}
-			}
-			break;
-		case TERMINATE_RECORD_TYPE:
-			break;
-		default: /* Should be trapped by DGifGetRecordType. */
-			break;
-		}
-	} while (RecordType != TERMINATE_RECORD_TYPE);
-
-	ColorMapObject *ColorMap = (gif->Image.ColorMap ? gif->Image.ColorMap
-	                                    : gif->SColorMap);
-	if (ColorMap == NULL) {
-		print_serial("Gif Image does not have a colormap\n");
-		exit(1);
-	}
-
-	/* check that the background color isn't garbage (SF bug #87) */
-	if (gif->SBackGroundColor < 0 ||
-	    gif->SBackGroundColor >= ColorMap->ColorCount) {
-		print_serial("Background color out of range for colormap\n");
-		exit(1);
-	}
-
-	unsigned char *Buffer, *BufferP;
-
-	GifRowType GifRow;
-	GifColorType *ColorMapEntry;
-	for(i = 0; i < gif->SHeight; i++){
-		GifRow = ScreenBuffer[i];
-		for(j = 0, BufferP = Buffer; j < gif->SWidth; j++){
-			if (GifRow[j] >= ColorMap->ColorCount){
-				continue;
-			}
-			ColorMapEntry = &ColorMap->Colors[GifRow[j]];
-			viewbuf[i*gif->SWidth + j] = make_color(ColorMapEntry->Red, ColorMapEntry->Green, ColorMapEntry->Blue, 0);
+	SavedImage *image = &gif->SavedImages[image_idx];
+	GifImageDesc *desc = &image->ImageDesc;
+	ColorMapObject *colorMap = desc->ColorMap ? desc->ColorMap : gif->SColorMap;
+	GraphicsControlBlock gcb;
+	int transparent_idx = -1;
+	if(DGifSavedExtensionToGCB(gif, image_idx, &gcb) == GIF_OK){
+		if(gcb.TransparentColor != NO_TRANSPARENT_COLOR){
+			transparent_idx = gcb.TransparentColor;
 		}
 	}
 
-	vp_funcs->copy(vp);
+
+	for(int y = 0; y < desc->Height; y++){
+		for(int x = 0; x < desc->Width; x++){
+			int c=  image->RasterBits[y * desc->Width + x];
+			if(colorMap){
+				GifColorType rgb;
+
+				if(gcb.DisposalMode == DISPOSE_BACKGROUND){
+					rgb = gif->SColorMap->Colors[gif->SBackGroundColor];
+					viewbuf[(y+desc->Top)*gif->SWidth + (x+desc->Left)] = make_color(rgb.Red, rgb.Green, rgb.Blue, 0);
+				}
+				else if(gcb.DisposalMode == DISPOSE_PREVIOUS){
+					continue;
+				}
+
+				if(transparent_idx != -1 && c == transparent_idx){
+					continue;
+				}
+				rgb = colorMap->Colors[c];
+				uint32_t color = make_color(rgb.Red, rgb.Green, rgb.Blue, 0);
+				viewbuf[(y+desc->Top)*gif->SWidth + (x+desc->Left)] = color;
+			}
+		}
+	}
 }
 
 int main(int argc, char **argv){
@@ -186,59 +104,34 @@ int main(int argc, char **argv){
 
 	int gif_err;
 	GifFileType *gif = DGifOpen(&Ifile, readgif, &gif_err);
-	if(gif_err != D_GIF_SUCCEEDED){
-		print_serial("[GIF] Error\n");
-		print_serial((char *) GifErrorString(gif_err));
-		print_serial("\n");
-
-		freeRegion(heap, REGION_SIZE);
-		return 1;
-	}
-	//gif_err = DGifSlurp(gif);
+	gif_err = DGifSlurp(gif);
 	
 	vp = vp_funcs->open(gif->SWidth, gif->SHeight, argv[1]);
 	viewbuf = malloc(gif->SWidth * gif->SHeight * 4);
 	vp_funcs->set_buffer(vp, viewbuf, gif->SWidth * gif->SHeight * 4);
 	vp_funcs->add_event_handler(vp, event_handler);
 
-	int i, j, Size;
+	uint32_t *timer_ticks = getTimerTickHandle();
 
-	if ((ScreenBuffer = (GifRowType *)malloc(gif->SHeight * sizeof(GifRowType))) == NULL) {
-		print_serial("Failed to allocate memory required, aborted.");
-		goto terminate;
-	}
-
-	Size = gif->SWidth * sizeof(GifPixelType); /* Size in bytes one row.*/
-	if ((ScreenBuffer[0] = (GifRowType)malloc(Size)) == NULL) { /* First row. */
-		print_serial("Failed to allocate memory required, aborted.");
-		goto terminate;
-	}
-
-	for (i = 0; i < gif->SWidth;
-	     i++) { /* Set its color to BackGround. */
-		ScreenBuffer[0][i] = gif->SBackGroundColor;
-	}
-	for (i = 1; i < gif->SHeight; i++) {
-		/* Allocate the other rows, and set their color to background
-		 * too: */
-		if ((ScreenBuffer[i] = (GifRowType)malloc(Size)) == NULL) {
-			print_serial("Failed to allocate memory required, aborted.");
-			goto terminate;
-		}
-
-		memcpy(ScreenBuffer[i], ScreenBuffer[0], Size);
-	}
-
-	GIF_CopyImageToViewport(gif, 0);
-
+	int image_idx = 0;
 	running = 1;
+	unsigned char last_tick = *timer_ticks;
 	while(running){
+		GIF_CopyImageToViewport(gif, image_idx);
+		vp_funcs->copy(vp);
 
+		image_idx++;
+		if(image_idx >= gif->ImageCount){
+			image_idx = 0;
+		}
+		//task_lock(0);
+		yield();
 	}
 	DGifCloseFile(gif, &gif_err);
 	terminate:;
 	vp_funcs->close(vp);
 	freeRegion(heap, REGION_SIZE);
+	print_serial("[GIF] Terminated\n");
 	return 0;
 }
 
