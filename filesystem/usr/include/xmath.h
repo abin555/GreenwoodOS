8ad293ee94991a21504159648a4d61b180d5670a
Aidan Boisvert <aidan.boisvert@gmail.com>
2025-02-16 17:25:32 -0600

    Reorg std lib

--- filesystem/usr/include/xmath.h
+++
@@ -1,9 +0,0 @@
-#ifndef XMATH_H
-#define XMATH_H
-
-#define _DFRAC ((1<<_DOFF)-1)
-#define _DMASK (0x7fff&!_DFRAC)
-#define _DMAX ((1<<(15-_DOFF))-1)
-#define _DNAN (0x8000|_DMAX<<_DOFF|1<<(_DOFF-1))
-
-#endif
