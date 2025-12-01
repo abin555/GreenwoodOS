d6cf22a0812864f5b97acab5444f6582840f306b
Aidan Boisvert <aidan.boisvert@gmail.com>
2025-11-30 22:21:03 -0600

    Tetris

--- filesystem/tetris/main.c
+++ filesystem/tetris/main.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <sys/vp.h>
 #include <sys/task.h>
+#include <sys/io.h>
 #include "tetris.h"
 #include "screen.h"
 
@@ -19,13 +20,36 @@ void fallEvent(){
     }
 }
 
+struct RealTimeClock {
+	unsigned char second;
+	unsigned char minute;
+	unsigned char hour;
+	unsigned char day;
+	unsigned char month;
+	unsigned int year;
+	unsigned int msec;
+};
+
+void gettime(int rtc_fd, int *sec, int *usec){
+    struct RealTimeClock rtc;
+    read(rtc_fd, &rtc, sizeof(rtc));
+    *sec = 60*rtc.minute + rtc.second;
+    *usec = rtc.msec;
+}
+
 int main(int argc, char **argv){
+    int rtc_fd = open("/-/dev/RTC", O_READ);
     printf("TETRIS\n");
     initScreen();
     tetris_init(&t);
 
     invertDisplay(1);
+
+    int sec, msec;
+    int lock = 0;
+    
     while(running){
+        //gettime(rtc_fd, &sec, &msec);
         clearDisplay();
         tetris_draw(&t);
         
@@ -51,7 +75,6 @@ int main(int argc, char **argv){
             window->ascii = '\0';
             tetris_move(&t, Right);
         }
-
         yield();
     }
 }
